706c9aa6cfbadb8596136bf7b205fd7e56a3fce8
rajvatsal <rajvatsal2004@gmail.com>
2024-01-11 22:48:54 +0530

    feat(options): Remove splitbelow

--- after/plugin/options.lua
+++ after/plugin/options.lua
@@ -3,5 +3,5 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
 vim.opt.relativenumber = true
-vim.opt.splitbelow = true
+-- vim.opt.splitbelow = true
 vim.cmd.colorscheme 'rose-pine-main' -- Set custom colorscheme [[ NOTE: Never set it to "onedark" cuz it breaks the editor ]]
