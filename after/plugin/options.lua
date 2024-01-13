ba8fb3e1c18f77498a529d2debfee2eeb45278f9
rajvatsal <rajvatsal2004@gmail.com>
2024-01-14 00:16:02 +0530

    feat(options): Set cursorline

--- after/plugin/options.lua
+++ after/plugin/options.lua
@@ -3,5 +3,6 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
 vim.opt.relativenumber = true
+vim.opt.cursorline = true
 -- vim.opt.splitbelow = true
 vim.cmd.colorscheme 'rose-pine-main' -- Set custom colorscheme [[ NOTE: Never set it to "onedark" cuz it breaks the editor ]]
