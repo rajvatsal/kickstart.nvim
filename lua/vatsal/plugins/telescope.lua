8ed228946efa59e75e986ed1df094ea260b77530
rajvatsal <rajvatsal2004@gmail.com>
2024-01-14 14:07:46 +0530

    refactor(alpha): Remove some ascii_arts

--- lua/vatsal/plugins/telescope.lua
+++ lua/vatsal/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
   'nvim-telescope/telescope.nvim',
   branch = '0.1.x',
   dependencies = {
-    'nvim-lua/plenary.nvim',
+    { 'nvim-lua/plenary.nvim' },
     -- Fuzzy Finder Algorithm which requires local dependencies to be built.
     -- Only load if `make` is available. Make sure you have the system
     -- requirements installed.
