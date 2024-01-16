d254d19a1d551dfeeaff8d5191c2904c11149d3d
rajvatsal <rajvatsal2004@gmail.com>
2024-01-16 21:35:40 +0530

    refactor(colorscheme): Set colorscheme in after/plugin

--- after/plugin/options.lua
+++ after/plugin/options.lua
@@ -4,4 +4,5 @@ vim.opt.expandtab = true
 vim.opt.softtabstop = 2
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
+vim.cmd('colorscheme rose-pine')
 -- vim.opt.splitbelow = true
--- lua/vatsal/plugins/colorschemes.lua
+++ lua/vatsal/plugins/colorschemes.lua
@@ -26,8 +26,5 @@ return {
 		name = 'rose-pine',
 		priority = 1000,
 		lazy = false,
-		config = function()
-			vim.cmd('colorscheme rose-pine')
-		end,
 	},
 }
