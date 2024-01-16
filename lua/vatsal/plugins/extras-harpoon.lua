77f8ebfed70784ceef31d1ecd60f53e1340b2081
rajvatsal <rajvatsal2004@gmail.com>
2024-01-16 18:38:49 +0530

    feat(plugins): Add harpoon

---
+++ lua/vatsal/plugins/extras-harpoon.lua
@@ -0,0 +1,24 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = {
+		{ '<S-m>', desc = 'Add file to harpoon' },
+		{ '<TAB>', desc = 'Toggle harpoon' },
+	},
+	config = function()
+		-- Set telescope as UI for harpoon
+		local harpoon = require('harpoon')
+		harpoon:setup({})
+
+		local keymap = vim.keymap.set
+		local opts = { noremap = true, silent = true }
+
+		keymap("n", "<S-m>", "<cmd>lua require('vatsal.plugins.extras-harpoon').mark_file()<cr>", opts)
+		keymap("n", "<TAB>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, opts)
+	end,
+	mark_file = function()
+		(function() require('harpoon'):list():append() end)()
+		vim.notify " 󱡅  marked file"
+	end,
+}
