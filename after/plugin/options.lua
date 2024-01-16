5623dd2f78efeeec402d1c3557bbf04a57241a07
rajvatsal <rajvatsal2004@gmail.com>
2024-01-16 17:09:23 +0530

    feat(plugins): Add neoscroll

--- after/plugin/options.lua
+++ after/plugin/options.lua
@@ -1,7 +1,7 @@
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.bo.softtabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 -- vim.opt.splitbelow = true
