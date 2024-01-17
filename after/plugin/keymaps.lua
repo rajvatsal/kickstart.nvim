c51ba92171d454472992a0946fefb87298771c14
rajvatsal <rajvatsal2004@gmail.com>
2024-01-17 18:45:45 +0530

    feat(plugins): Use toggleterm for terminal

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -4,5 +4,5 @@ vim.keymap.set('n', '<C-h>', ':bprev<CR>', { noremap = true, silent = true, desc
 vim.keymap.set('n', '<C-l>', ':bnext<CR>', { noremap = true, silent = true, desc = 'Open next buffer' })
 vim.keymap.set('n', '<leader>m', ':cd %:p:h<CR>', { noremap = true, silent = true, desc = 'Move to current buffer' })
 vim.keymap.set('x', 'p', [["_dp]]) -- Don't update register when you paste over a word
-vim.keymap.set('n', '`', '<cmd>botright split +term | resize 10<CR>' .. rmv_lines, { noremap = true, silent = true })
+vim.keymap.set('n', '`', '<cmd>ToggleTerm .| resize 10<CR>' .. rmv_lines, { noremap = true, silent = true })
 vim.keymap.set('t', '`', '<cmd>:q<CR>', { noremap = true, silent = true })
