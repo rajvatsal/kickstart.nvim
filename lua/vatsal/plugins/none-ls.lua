086de37af7cb7675bfac11c58e1e01fa9856973c
Vatsal <rajvatsal2004@gmail.com>
2024-01-27 21:20:45 +0530

    style(none-ls): Format with stylua

--- lua/vatsal/plugins/none-ls.lua
+++ lua/vatsal/plugins/none-ls.lua
@@ -2,14 +2,14 @@ return {
   'nvimtools/none-ls.nvim',
   ft = { 'lua', 'javascript', 'typescript', 'c', 'python', 'rust', 'go', 'zig', 'html', 'css', 'markdown', 'jsx' },
   opts = function(_, opts)
-    local null_ls = require('null-ls')
+    local null_ls = require 'null-ls'
     opts.sources = {
       null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.prettier.with({
-        disabled_filetypes = { 'javascript', 'typescript', 'json' }   -- Use biome
-      }),
+      null_ls.builtins.formatting.prettier.with {
+        disabled_filetypes = { 'javascript', 'typescript', 'json' }, -- Use biome
+      },
       null_ls.builtins.formatting.biome,
       -- null_ls.builtins.diagnostics.biome,
     }
-  end
+  end,
 }
