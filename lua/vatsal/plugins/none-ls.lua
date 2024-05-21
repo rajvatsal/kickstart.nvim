47b0f76aaaab3d4c5da5e5ffd91c36ea9fc317fe
vd <rajvatsal2004@gmail.com>
2024-05-21 17:16:52 +0530

    refactor: Switch to eslint and preetier

--- lua/vatsal/plugins/none-ls.lua
+++ lua/vatsal/plugins/none-ls.lua
@@ -2,13 +2,10 @@ return {
   'nvimtools/none-ls.nvim',
   ft = { 'lua', 'javascript', 'typescript', 'c', 'python', 'rust', 'go', 'zig', 'html', 'css', 'markdown', 'jsx' },
   opts = function(_, opts)
-    local null_ls = require 'null-ls'
+    local null_ls = require 'null-ls.builtins'
     opts.sources = {
-      null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.prettier.with {
-        disabled_filetypes = { 'javascript', 'typescript', 'json', 'lua' }, -- Use biome, stylua
-      },
-      null_ls.builtins.formatting.biome,
+      null_ls.formatting.stylua,
+      null_ls.formatting.prettier,
     }
   end,
 }
--- lua/vatsal/plugins/nvim-lspconfig.lua
+++ lua/vatsal/plugins/nvim-lspconfig.lua
@@ -10,7 +10,7 @@ return {
 
     -- Useful status updates for LSP
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-    { 'j-hui/fidget.nvim', opts = {} },
+    { 'j-hui/fidget.nvim',       opts = {} },
 
     -- Additional lua configuration, makes nvim stuff amazing!
     'folke/neodev.nvim',
@@ -91,7 +91,9 @@ return {
           -- diagnostics = { disable = { 'missing-fields' } },
         },
       },
-      biome = {},
+      eslint = {
+        filetypes = { 'javascript', 'typescript' },
+      },
     }
 
     -- Setup neovim lua configuration
