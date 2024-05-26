eda3842828bc80b2e0c130e1ad4c5112ea657205
vd <rajvatsal2004@gmail.com>
2024-05-26 16:59:04 +0530

    use prettier

--- lua/vatsal/plugins/none-ls.lua
+++ lua/vatsal/plugins/none-ls.lua
@@ -8,15 +8,7 @@ return {
     local null_ls = require 'null-ls.builtins'
     opts.sources = {
       null_ls.formatting.stylua,
-      null_ls.formatting.prettierd.with {
-        disabled_filetypes = { 'javascript', 'typescript', 'typescriptreact', 'jsx', 'tsx' },
-      },
-      null_ls.formatting.biome,
-      -- eslint_d deosn't work in windows
-      -- require 'none-ls.code_actions.eslint_d',
-      -- require 'none-ls.diagnostics.eslint_d',
-      --   root_dir = require('lspconfig').util.root_pattern 'eslint.config.mjs',
-      -- },
+      null_ls.formatting.prettierd,
     }
   end,
 }
