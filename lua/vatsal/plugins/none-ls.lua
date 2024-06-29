287f62dc06166b46622c14be281a993936a91ec9
vd <rajvatsal2004@gmail.com>
2024-06-29 18:57:09 +0530

    fix(none-ls): Use prettierd on windows and biome on linux

--- lua/vatsal/plugins/none-ls.lua
+++ lua/vatsal/plugins/none-ls.lua
@@ -6,17 +6,20 @@ return {
   ft = { 'lua', 'javascript', 'typescript', 'c', 'python', 'rust', 'go', 'zig', 'html', 'css', 'markdown', 'jsx' },
   opts = function(_, opts)
     local null_ls = require 'null-ls.builtins'
+
     opts.sources = {
       null_ls.formatting.stylua,
-      null_ls.formatting.prettierd.with {
-        disabled_filetypes = { 'javascript', 'typescript', 'typescriptreact', 'jsx', 'tsx' },
-      },
-      null_ls.formatting.biome,
-      -- eslint_d deosn't work in windows
-      -- require 'none-ls.code_actions.eslint_d',
-      -- require 'none-ls.diagnostics.eslint_d',
-      --   root_dir = require('lspconfig').util.root_pattern 'eslint.config.mjs',
-      -- },
     }
+    if vim.fn.has 'windows' then
+      table.insert(opts.sources, null_ls.formatting.prettierd)
+    else
+      table.insert(
+        opts.sources,
+        null_ls.formatting.prettierd.with {
+          disabled_filetypes = { 'javascript', 'typescript', 'typescriptreact', 'jsx', 'tsx' },
+        }
+      )
+      table.insert(opts.sources, null_ls.formatting.biome)
+    end
   end,
 }
