5964718d70fce566fb68a1d3ca9d1f1bfa2c0a06
Vatsal <rajvatsal2004@gmail.com>
2024-07-03 19:54:11 +0530

    format(colorschemes): add formatting

--- lua/vatsal/plugins/colorschemes.lua
+++ lua/vatsal/plugins/colorschemes.lua
@@ -1,57 +1,57 @@
 return {
-	--Gruvbox colorscheme
-	{ "ellisonleao/gruvbox.nvim",  priority = 1000, },
+  --Gruvbox colorscheme
+  { 'ellisonleao/gruvbox.nvim',  priority = 1000 },
 
-	--tokyonight colorscheme
-	{ "folke/tokyonight.nvim",     priority = 1000, },
+  --tokyonight colorscheme
+  { 'folke/tokyonight.nvim',     priority = 1000 },
 
-	-- lunar-vim colorschemes
-	{ "lunarvim/colorschemes",     priority = 1000, },
+  -- lunar-vim colorschemes
+  { 'lunarvim/colorschemes',     priority = 1000 },
 
-	-- kanagawa
-	{ "rebelot/kanagawa.nvim",     priority = 1000, },
+  -- kanagawa
+  { 'rebelot/kanagawa.nvim',     priority = 1000 },
 
-	-- latte, frappe, macchiato, mocha
-	{ "catppuccin/nvim",           priority = 1000, },
+  -- latte, frappe, macchiato, mocha
+  { 'catppuccin/nvim',           priority = 1000 },
 
-	-- Theme inspired by Atom
-	{ 'navarasu/onedark.nvim',     priority = 1000, },
+  -- Theme inspired by Atom
+  { 'navarasu/onedark.nvim',     priority = 1000 },
 
-	-- Nightfox
-	{ 'EdenEast/nightfox.nvim',    priority = 1000, },
+  -- Nightfox
+  { 'EdenEast/nightfox.nvim',    priority = 1000 },
 
-	--Rosepine (Primeagen)
-	{ 'rose-pine/neovim',          priority = 1000, name = 'rose-pine', },
+  --Rosepine (Primeagen)
+  { 'rose-pine/neovim',          priority = 1000, name = 'rose-pine' },
 
-	-- Dracula
-	{ 'Mofiqul/dracula.nvim',      priority = 1000, },
+  -- Dracula
+  { 'Mofiqul/dracula.nvim',      priority = 1000 },
 
-	-- Edge
-	{ 'sainnhe/edge',              priority = 1000, },
+  -- Edge
+  { 'sainnhe/edge',              priority = 1000 },
 
-	-- Sonokai
-	{ 'sainnhe/sonokai',           priority = 1000, },
+  -- Sonokai
+  { 'sainnhe/sonokai',           priority = 1000 },
 
-	-- Everforest
-	{ 'sainnhe/everforest',        priority = 1000, },
+  -- Everforest
+  { 'sainnhe/everforest',        priority = 1000 },
 
-	-- OnedarkPro
-	{ 'olimorris/onedarkpro.nvim', priority = 1000, },
+  -- OnedarkPro
+  { 'olimorris/onedarkpro.nvim', priority = 1000 },
 
-	-- Monokai
-	{ 'ku1ik/vim-monokai',         priority = 1000, },
+  -- Monokai
+  { 'ku1ik/vim-monokai',         priority = 1000 },
 
-	-- Material
-	{
-		'marko-cerovac/material.nvim',
-		priority = 1000,
-		lazy = false,
-		opts = function(_, opts)
-			opts.lualine_style = 'stealth'
-			vim.g.material_style = 'deep ocean'
-			vim.cmd('colorscheme material')
-		end,
-	},
+  -- Material
+  {
+    'marko-cerovac/material.nvim',
+    priority = 1000,
+    lazy = false,
+    opts = function(_, opts)
+      opts.lualine_style = 'stealth'
+      -- vim.g.material_style = 'deep ocean'
+      vim.cmd.colorscheme 'material-deep-ocean'
+    end,
+  },
 
-	{ 'nyoom-engineering/oxocarbon.nvim', priority = 1000, },
+  { 'nyoom-engineering/oxocarbon.nvim', priority = 1000 },
 }
