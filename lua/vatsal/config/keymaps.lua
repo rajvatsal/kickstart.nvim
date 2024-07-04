0678ad3b550967c7bf566eb8baa44bd43696edcf
vd <rajvatsal2004@gmail.com>
2024-07-04 21:31:49 +0530

    feat(keymaps): Move to js file's root
    Add a keymap <leader>jsr to move to javascript's root directory which
    usually is the one containing package.json

--- lua/vatsal/config/keymaps.lua
+++ lua/vatsal/config/keymaps.lua
@@ -19,7 +19,36 @@ local rmv_lines = '<cmd>lua vim.wo.number=false<CR><cmd>lua vim.wo.relativenumbe
 vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true }) -- Set keymap to exit terminal using 'exit'
 vim.keymap.set('n', '<C-h>', ':bprev<CR>', { noremap = true, silent = true, desc = 'Open previous buffer' })
 vim.keymap.set('n', '<C-l>', ':bnext<CR>', { noremap = true, silent = true, desc = 'Open next buffer' })
-vim.keymap.set('n', '<leader>m', ':cd %:p:h<CR>:echo "Moved to current buffer"<CR>', { noremap = true, silent = true, desc = 'Move to current buffer' })
+vim.keymap.set(
+  'n',
+  '<leader>mb',
+  ':cd %:p:h<CR>:echo "Moved to current buffer path"<CR>',
+  { noremap = true, silent = true, desc = "[M]ove to current [B]uffer's path" }
+)
 vim.keymap.set('x', 'p', [["_dp]]) -- Don't update register when you paste over a word
 vim.keymap.set('n', '`', '<cmd>ToggleTerm .| resize 10<CR>' .. rmv_lines, { noremap = true, silent = true })
 vim.keymap.set('t', '`', '<cmd>:q<CR>', { noremap = true, silent = true })
+
+-- move to the root of file sytem in js projects typically where package.json exists
+local function gotoroot()
+  local path = vim.api.nvim_buf_get_name(0)
+  local jsonPath
+  while path and path ~= vim.fn.fnamemodify(path, ':p:h:h') do
+    if vim.fn.has 'win32' == 1 then
+      jsonPath = path .. '\\package.json'
+    else
+      jsonPath = path .. '/package.json'
+    end
+
+    if vim.fn.filereadable(jsonPath) == 1 then
+      vim.cmd('cd ' .. path)
+      print('Move to root of the file ' .. path)
+      return
+    end
+    path = vim.fn.fnamemodify(path, ':h')
+  end
+
+  print 'package.json not found in the file tree'
+end
+
+vim.keymap.set('n', '<leader>jsr', gotoroot, { silent = true, noremap = true, desc = 'Move to [J]ava[S]cript [R]oot' })
