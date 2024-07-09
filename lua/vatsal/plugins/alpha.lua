261f034e85b1bf4e43a4115e5c529f074d6c7939
vd <rajvatsal2004@gmail.com>
2024-07-09 22:18:32 +0530

    refactor(alpha): New config with stratify

--- lua/vatsal/plugins/alpha.lua
+++ lua/vatsal/plugins/alpha.lua
@@ -1,5 +1,49 @@
 local _ascii_arts = require 'vatsal.misc.ascii-arts'
 
+local function oldConfig()
+  local alpha = require 'alpha'
+  local dashboard = require 'alpha.themes.dashboard'
+
+  -- Set header
+  dashboard.section.header.val = _ascii_arts['f_society']
+
+  -- Set menu
+  dashboard.section.buttons.val = {
+    dashboard.button('e', '  New file', ':ene <BAR> startinsert <CR>'),
+    dashboard.button('r', '  Recently used files', ':Telescope oldfiles<CR>'),
+    dashboard.button('f', '  Find file', ':Telescope find_files<CR>'),
+    dashboard.button('t', '  Find text', ':Telescope live_grep <CR>'),
+    dashboard.button('q', '  Quit NVIM', ':qa<CR>'),
+  }
+
+  -- Set footer
+  -- dashboard.section.footer.val = require('alpha.fortune')()
+
+  -- Colors
+
+  dashboard.section.header.opts.hl = 'String'
+  dashboard.section.buttons.opts.hl = 'Debug'
+  dashboard.section.footer.opts.hl = 'white'
+  dashboard.config.opts.noautocmd = true
+
+  vim.cmd [[autocmd User AlphaReady echo 'ready']]
+
+  alpha.setup(dashboard.opts)
+
+  -- Plugins loaded footer
+  vim.api.nvim_create_autocmd('User', {
+    once = true,
+    pattern = 'LazyVimStarted',
+    callback = function()
+      local stats = require('lazy').stats()
+      local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+      dashboard.section.footer.val = '⚡Neovim loaded ' ..
+      stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms'
+      pcall(vim.cmd.AlphaRedraw)
+    end,
+  })
+end
+
 return {
   'goolord/alpha-nvim',
   cond = function()
@@ -12,45 +56,42 @@ return {
   event = 'VimEnter',
   cmd = 'Alpha',
   config = function()
+    local fortune = require 'alpha.fortune'
     local alpha = require 'alpha'
-    local dashboard = require 'alpha.themes.dashboard'
+    local startify = require 'alpha.themes.startify'
 
-    -- Set header
-    dashboard.section.header.val = _ascii_arts['f_society']
+    startify.section.header.val = _ascii_arts.afo_light
 
-    -- Set menu
-    dashboard.section.buttons.val = {
-      dashboard.button('e', '  New file', ':ene <BAR> startinsert <CR>'),
-      dashboard.button('r', '  Recently used files', ':Telescope oldfiles<CR>'),
-      dashboard.button('f', '  Find file', ':Telescope find_files<CR>'),
-      dashboard.button('t', '  Find text', ':Telescope live_grep <CR>'),
-      dashboard.button('q', '  Quit NVIM', ':qa<CR>'),
+    startify.config.layout[1].val = 2
+    startify.config.layout[3].val = 2
+
+    startify.section.top_buttons.val = {
+      startify.button('e', '  New file', ':ene <BAR> startinsert <CR>'),
+      startify.button('r', '  Recently used files', ':Telescope oldfiles<CR>'),
+      startify.button('t', '󰈞  Find text', ':Telescope live_grep <CR>'),
     }
 
-    -- Set footer
-    -- dashboard.section.footer.val = require('alpha.fortune')()
+    -- disable MRU
+    startify.section.mru.val = { { type = 'padding', val = 0 } }
+    -- disable MRU cwd
+    startify.section.mru_cwd.val = { { type = 'padding', val = 0 } }
+    -- disable nvim_web_devicons
+    startify.nvim_web_devicons.enabled = false
+    -- startify.nvim_web_devicons.highlight = false
+    -- startify.nvim_web_devicons.highlight = 'Keyword'
 
-    -- Colors
-    vim.api.nvim_set_hl(0, 'footer.clr', { fg = '#ffff00' })
-    dashboard.section.header.opts.hl = 'String'
-    dashboard.section.buttons.opts.hl = 'Debug'
-    dashboard.section.footer.opts.hl = 'white'
-    dashboard.config.opts.noautocmd = true
+    startify.section.bottom_buttons.val = {
+      startify.button('q', '󰅚  Quit NVIM', ':qa<CR>'),
+      { type = 'padding', val = 2 },
+    }
 
-    vim.cmd [[autocmd User AlphaReady echo 'ready']]
+    vim.api.nvim_set_hl(0, 'FooterStartify', { fg = '#8A94BC' })
+    startify.section.footer.val = { { type = 'text', val = fortune(), opts = { hl = 'FooterStartify', margin = 30 } } }
 
-    alpha.setup(dashboard.opts)
-
-    -- Plugins loaded footer
-    vim.api.nvim_create_autocmd('User', {
-      once = true,
-      pattern = 'LazyVimStarted',
-      callback = function()
-        local stats = require('lazy').stats()
-        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-        dashboard.section.footer.val = '⚡Neovim loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms'
-        pcall(vim.cmd.AlphaRedraw)
-      end,
-    })
+    -- ignore filetypes in MRU
+    startify.mru_opts.ignore = function(path, ext)
+      return (string.find(path, 'COMMIT_EDITMSG')) or (vim.tbl_contains(default_mru_ignore, ext))
+    end
+    alpha.setup(startify.config)
   end,
 }
