acaaf8edc5e8ed185d7a328c9d91aa8e2087d486
vd <rajvatsal2004@gmail.com>
2024-07-25 17:49:48 +0530

    feat(whichkey): Move command
    - Add '<leader>m' '[M]ove' for move command shortcuts

--- lua/vatsal/plugins/which-key.lua
+++ lua/vatsal/plugins/which-key.lua
@@ -9,6 +9,7 @@ return {
       ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
       ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
       ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+      ['<leader>m'] = { name = '[M]ove', _ = 'which_key_ignore' },
       ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
@@ -21,5 +22,5 @@ return {
       ['<leader>'] = { name = 'VISUAL <leader>' },
       ['<leader>h'] = { 'Git [H]unk' },
     }, { mode = 'v' })
-  end
+  end,
 }
