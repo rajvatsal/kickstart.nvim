8c0ec1427b2b585e1147e839628016081db62363
vatsal <rajvatsal2004@gmail.com>
2024-08-24 12:59:38 +0530

    refactor(debug): Lazy load on <F5>

--- lua/vatsal/plugins/debug.lua
+++ lua/vatsal/plugins/debug.lua
@@ -17,6 +17,9 @@ return {
     'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
   },
+  keys = {
+    { '<F5>', "<cmd>lua require('dap').continue()<CR>", { desc = 'Debug: Start/Continue' } },
+  },
   config = function()
     local dap = require 'dap'
     local dapui = require 'dapui'
@@ -54,7 +57,7 @@ return {
     }
 
     -- Basic debugging keymaps, feel free to change to your liking!
-    vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+    -- vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
     vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
     vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
     vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
