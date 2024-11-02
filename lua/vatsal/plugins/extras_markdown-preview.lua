b7f38a6853645823c0e79cba089d379db8e5e063
vatsal <rajvatsal2004@gmail.com>
2024-11-02 12:35:20 +0530

    fix(markdown-preview): MarkDownToggle cmd not found

--- lua/vatsal/plugins/extras_markdown-preview.lua
+++ lua/vatsal/plugins/extras_markdown-preview.lua
@@ -1,5 +1,7 @@
 return {
   'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  ft = { 'markdown' },
   keys = {
     { '<leader>tm', '<cmd>MarkdownPreviewToggle<CR>', desc = 'toggle markdown preview' },
   },
