a9136e84e4050d2f63adfbb00d387b5fc4cecc8f
vatsal <rajvatsal2004@gmail.com>
2024-11-26 14:40:42 +0530

    refactor(alpha): Disable alpha loading screen

--- lua/vatsal/plugins/alpha.lua
+++ lua/vatsal/plugins/alpha.lua
@@ -2,13 +2,7 @@ local configs = require 'vatsal.misc.alpha-settings'
 
 return {
   'goolord/alpha-nvim',
-  cond = function()
-    if vim.fn.argv(0) == '' then
-      return true
-    else
-      return false
-    end
-  end,
+  enabled = false,
   event = 'VimEnter',
   cmd = 'Alpha',
   config = configs['alpha_startify_config'],
