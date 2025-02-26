5a0aa016ddef94abf52f09d31e840ea5cb98227e
vatsal <rajvatsal2004@gmail.com>
2025-02-26 22:54:38 +0530

    refactor(none-ls): switch to eslint from biome

--- lua/vatsal/plugins/none-ls.lua
+++ lua/vatsal/plugins/none-ls.lua
@@ -5,26 +5,22 @@ local clang = { filetypes = { 'c' } }
 return {
   'nvimtools/none-ls.nvim',
   dependencies = {
-    'nvimtools/none-ls-extras.nvim',
+    "nvimtools/none-ls-extras.nvim",
   },
   event = { 'BufReadPre', 'BufNewFile' },
   opts = {},
-  config = function(_, opts)
+  config = function()
     local nls = require 'null-ls.builtins'
 
-    opts.sources = {
-      nls.diagnostics.stylelint,
-      nls.formatting.stylua,
-      nls.formatting.biome.with(biome),
-      nls.formatting.prettierd.with(prettierd),
-      nls.formatting.clang_format.with(clang),
-    }
-
-    if vim.fn.has 'win32' == 1 then
-      table.insert(opts.sources, nls.formatting.prettierd)
-      table.insert(opts.source, nls.diagonstics.eslint_d)
-    end
-
-    require('null-ls').setup(opts)
+    require('null-ls').setup({
+      sources = {
+        nls.diagnostics.stylelint,
+        nls.formatting.stylua,
+        nls.formatting.biome.with(biome),
+        nls.formatting.prettierd.with(prettierd),
+        nls.formatting.clang_format.with(clang),
+        require('none-ls.diagnostics.eslint_d'),
+      }
+    })
   end,
 }
