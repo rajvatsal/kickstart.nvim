9d8c9120270bc741717bce95f6dc400db4d7f26a
vatsal <rajvatsal2004@gmail.com>
2025-05-23 11:34:15 +0530

    refactor(init.lua): remove commented rtp plugins

--- init.lua
+++ init.lua
@@ -24,16 +24,8 @@ require('lazy').setup(config.plugins, {
   defaults = { lazy = true },
   performance = {
     rtp = {
-      -- disable some rtp plugins
       disabled_plugins = {
-        -- 'gzip',
-        -- 'matchit',
-        -- 'matchparen',
-        -- 'netrwPlugin',
-        -- 'tarPlugin',
-        -- 'tohtml',
         'tutor',
-        -- 'zipPlugin',
       },
     },
   },
